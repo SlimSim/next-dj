561576560bd0c2c93db87e5c9cf08c5f388e9d9c
SlimSim <slimsimapps@gmail.com>
2024-10-04 20:45:04 +0200

    fix: fixing correct path for pwa sw- workbox-files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const withPWA = require('next-pwa')({
-  dest: 'public',
+  dest: 'public/service-workers',
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
