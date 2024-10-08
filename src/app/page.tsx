c64ca4b7221cfafaa6578ffadb0f198291b1517b
SlimSim <slimsimapps@gmail.com>
2024-10-08 22:06:14 +0200

    chore: updating project_structure and todo-list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ const HomePage = () => {
       <h2 className="text-xl">Welcome to Next DJ</h2>
       <p>Your music player PWA.</p>
       <p>Jag vill ha Sonner</p>
-      <ul>
+      <ul className="list-decimal ml-4">
         <li>
           att fixa: att den kan ladda ner och spara låtar som finns på servern,
           ska jag göra detta via backenden kanske? (så att NextJs anropar
@@ -42,6 +42,31 @@ const HomePage = () => {
           sparar det på disk (och där finns ju BÅDE den publika disken, som
           chrome komemr åt, men också en sån där seecret place, som ios kommer
           åt) eller i IndexedDB!)
+          <ul className="list-disc ml-4">
+            <li>
+              denan fil (take me to church) från firebase:
+              4ed171cc22759ea60cfe6049f01f0e1b8715e52acbc1b3bffe88acc1c42652a5
+            </li>
+            <li>
+              har denna länk:{" "}
+              <a
+                target="_blank"
+                href="https://firebasestorage.googleapis.com/v0/b/troff-test.appspot.com/o/TroffFiles%2F4ed171cc22759ea60cfe6049f01f0e1b8715e52acbc1b3bffe88acc1c42652a5?alt=media&token=bfc80084-a477-4db6-b587-2ae00e64d5ac"
+              >
+                länk
+              </a>
+            </li>
+            <li>
+              testa att ladda ner den både till <b>indexedDB</b> OCH till{" "}
+              <b>fileSystemAPI</b>
+              (hemliga helst!)
+            </li>
+          </ul>
+        </li>
+        <li>Att ladda upp låtar från datorn också!</li>
+        <li>
+          Slå ihop tailwind.config.ts och tailwind.config.js, borde bara behöva
+          en eller?
         </li>
       </ul>
       <AudioPlayer nowPlaying={nowPlaying} />
