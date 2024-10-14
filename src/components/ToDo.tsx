dc475752dd6ed96ac15695fa117435dcf7367f65
SlimSim <slimsimapps@gmail.com>
2024-10-14 23:30:38 +0200

    docs: updating todo

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -12,39 +12,33 @@ const ToDo = React.forwardRef(() => {
         IndexedDB!)
         <ul className="list-disc ml-4">
           <li>
-            denan fil (take me to church) från firebase:
-            4ed171cc22759ea60cfe6049f01f0e1b8715e52acbc1b3bffe88acc1c42652a5
-          </li>
-          <li>
-            har denna länk:{" "}
             <a
+              className="underline"
               target="_blank"
               href="https://firebasestorage.googleapis.com/v0/b/troff-test.appspot.com/o/TroffFiles%2F4ed171cc22759ea60cfe6049f01f0e1b8715e52acbc1b3bffe88acc1c42652a5?alt=media&token=bfc80084-a477-4db6-b587-2ae00e64d5ac"
             >
-              länk
-            </a>
+              Take Me To Church
+            </a>{" "}
+            från firebase:
+            4ed171cc22759ea60cfe6049f01f0e1b8715e52acbc1b3bffe88acc1c42652a5
           </li>
           <li>
-            denan fil (Jurassic park) från firebase:
-            94b0acbaab70a53cf8ab7a6e81820bfe9fe473b27a819a22e198b480fb5690df
-          </li>
-          <li>
-            har denna länk:{" "}
             <a
+              className="underline"
               target="_blank"
               href="https://firebasestorage.googleapis.com/v0/b/troff-test.appspot.com/o/TroffFiles%2F94b0acbaab70a53cf8ab7a6e81820bfe9fe473b27a819a22e198b480fb5690df?alt=media&token=24d93957-021c-42d4-ae30-7ae180230b4e"
             >
-              Jurassic park
-            </a>
+              Jurassic Park
+            </a>{" "}
+            från firebase:
+            94b0acbaab70a53cf8ab7a6e81820bfe9fe473b27a819a22e198b480fb5690df
           </li>
-          <li>
-            testa att ladda ner den både till <b>indexedDB</b> OCH till{" "}
-            <b>fileSystemAPI</b>
-            (hemliga helst!)
-          </li>
+          <li>ladda ner dom till indexedDB</li>
         </ul>
       </li>
-      <li>Att ladda upp låtar från datorn också!</li>
+      <li>
+        <b>Att ladda upp låtar från datorn också!</b>
+      </li>
       <li>
         Slå ihop tailwind.config.ts och tailwind.config.js, borde bara behöva en
         eller?
