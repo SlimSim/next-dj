f9e668a6653f52176e7c2ba9f5cd586ae74dee47
SlimSim <slimsimapps@gmail.com>
2024-12-20 11:33:45 +0100

    fix: removin debug y

--- src/components/current-song-info.tsx
+++ src/components/current-song-info.tsx
@@ -1,6 +1,6 @@
-import { MusicMetadata } from '@/lib/types';
-import { formatTime } from '@/lib/utils';
-import React from 'react';
+import { MusicMetadata } from "@/lib/types";
+import { formatTime } from "@/lib/utils";
+import React from "react";
 
 interface CurrentSongInfoProps {
   track: MusicMetadata | null;
@@ -8,37 +8,38 @@ interface CurrentSongInfoProps {
   duration: number;
 }
 
-const CurrentSongInfo: React.FC<CurrentSongInfoProps> = ({ track, duration, currentTime }) => {
+const CurrentSongInfo: React.FC<CurrentSongInfoProps> = ({
+  track,
+  duration,
+  currentTime,
+}) => {
   return (
     <>
-    {track?.coverArt && (
-    <img
-      src={track.coverArt}
-      alt={track.title}
-      width={40}
-      height={40}
-      className="aspect-square rounded-md object-cover"
-    />
-    )}
-    <div className="space-y-1">
-        <h3 className="text-sm font-medium leading-none"> 
-        {track?.title || 'No track playing'}
+      {track?.coverArt && (
+        <img
+          src={track.coverArt}
+          alt={track.title}
+          width={40}
+          height={40}
+          className="aspect-square rounded-md object-cover"
+        />
+      )}
+      <div className="space-y-1">
+        <h3 className="text-sm font-medium leading-none">
+          {track?.title || "No track playing"}
         </h3>
         <div className="flex items-center text-xs text-muted-foreground">
-        <span>{track?.artist || 'Unknown artist'}</span>
-        <span className="mx-2">•</span>
-        <span>{formatTime(currentTime)}</span>
-        <span className="mx-1">/</span>
-        <span>y {formatTime(duration)}</span>
-        <span className="mx-2">•</span>
-        <span>-{formatTime(duration - currentTime)}</span>
+          <span>{track?.artist || "Unknown artist"}</span>
+          <span className="mx-2">•</span>
+          <span>{formatTime(currentTime)}</span>
+          <span className="mx-1">/</span>
+          <span>{formatTime(duration)}</span>
+          <span className="mx-2">•</span>
+          <span>-{formatTime(duration - currentTime)}</span>
         </div>
-    </div>
+      </div>
     </>
-  )
-
+  );
 };
 
 export default CurrentSongInfo;
-
-
