1a6fff022fa16da4edeba5826d9ed6406107c0e6
SlimSim <slimsimapps@gmail.com>
2025-01-07 10:21:42 +0100

    chore: update db version number

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const DB_CONFIG = {
-    MUSIC_DB_NAME: 'music-player-db',
-    MUSIC_DB_VERSION: 1,
-    HANDLES_DB_NAME: 'next-dj',
-    HANDLES_DB_VERSION: 1
-  } as const
+  MUSIC_DB_NAME: "music-player-db",
+  MUSIC_DB_VERSION: 2,
+  HANDLES_DB_NAME: "next-dj",
+  HANDLES_DB_VERSION: 1,
+} as const;
