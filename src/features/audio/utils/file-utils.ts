a6ec099c17ee04948eeeb770c5b4bd14390a61ae
SlimSim <slimsimapps@gmail.com>
2025-01-11 20:34:47 +0100

    feat: #16 more file formats!

--- src/features/audio/utils/file-utils.ts
+++ src/features/audio/utils/file-utils.ts
@@ -1,4 +1,16 @@
 export function isAudioFile(file: File): boolean {
-    const audioTypes = ['audio/mpeg', 'audio/wav', 'audio/mp4', 'audio/aac']
+    const audioTypes = [
+      'audio/mpeg',        // MP3
+      'audio/wav',         // WAV
+      'audio/mp4',         // M4A, MP4 Audio
+      'audio/aac',         // AAC
+      'audio/flac',        // FLAC
+      'audio/ogg',         // OGG, OGA
+      'audio/opus',        // OPUS
+      'audio/webm',        // WEBM Audio
+      'audio/x-m4a',       // M4A (alternative MIME type)
+      'audio/x-aiff',      // AIFF
+      'audio/x-wav'        // WAV (alternative MIME type)
+    ]
     return audioTypes.includes(file.type)
   }
